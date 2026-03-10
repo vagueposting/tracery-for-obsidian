6a27df4bb85d121bd5292425bb28586a2e132d6b
vagueposting <vagueposted@gmail.com>
2026-03-10 21:18:39 +0800

    refactor: removed unnecessary commands

--- src/main.ts
+++ src/main.ts
@@ -9,32 +9,6 @@ export default class TraceryForObsidian extends Plugin {
         await this.loadSettings();
 
         this.addSettingTab(new GrammarFolderLocation(this.app, this));
-
-        this.addCommand({ // TODO: I'll probably have to remove/alter this
-            // since I allow both YAML and JSON
-            id: 'check-yaml-block',
-            name: 'Check YAML block in current file',
-            checkCallback: (checking: boolean) => {
-                const activeFile = this.app.workspace.getActiveFile();
-                if (activeFile) {
-                    if (!checking) {
-                        this.runDataCheck(activeFile);
-                    }
-                    return true;
-                }
-                return false;
-            }
-        });
-    }
-
-    async runDataCheck(file: TFile) {
-        const data = await parseDataFromFolder(this.app, this.settings.grammarPath);
-        if (data) {
-            console.log("Found YAML data:", data);
-            new Notice("YAML block parsed successfully!");
-        } else {
-            new Notice("No valid YAML block found.");
-        }
     }
 
     onunload() { }
